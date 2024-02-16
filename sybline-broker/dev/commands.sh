4385d59943f642b05515bcc3adc9632d01c5bc10
Jake C <jakeychap@gmail.com>
2024-02-16 20:06:15 +0000

    sybline broker now provides default ingress

--- sybline-broker/dev/commands.sh
+++ sybline-broker/dev/commands.sh
@@ -1,2 +1,3 @@
 kubectl patch deployment kubernetes-dashboard -n kubernetes-dashboard --type 'json' -p '[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--enable-skip-login"}]
 kubectl -n kubernetes-dashboard create token admin-user
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission --namespace=ingress-nginx
